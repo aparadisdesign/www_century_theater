c62f487bb97aff2b5700add45e4e6d7535babbe6
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-10 18:17:05 -0400

    Under Construction Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>The Century Theater</h1>
+        <h2>Fort Kent, Maine</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We are in the process of building our site, for now we are posting all our showtime on facebook.
+          <br /><br />
+          Facebook messenger is also the best way to contact us.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://www.facebook.com/TheCenturyTheater"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Our Facebook Page
         </a>
       </header>
     </div>
