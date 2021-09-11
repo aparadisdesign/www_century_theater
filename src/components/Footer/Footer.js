16351aa2f26846b57239ff67ccb2fb9c0ce0500e
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-11 18:50:36 -0400

    Social Link Refactor into a component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,8 @@
 import React, {  } from 'react';
 import './Footer.css';
 import {Container} from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import {library} from "@fortawesome/fontawesome-svg-core";
+import SocialLink from "./SocialLink/SocialLink";
 
-library.add(fab)
-
-const iconStyle = {
-    padding: `0 15px`,
-}
 
 const Footer = () => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
@@ -18,12 +11,10 @@ const Footer = () => {
             <Container className="container">
                 <span className="text-muted">© 2021 The Century Theater, All rights reserved.</span>
                 <span style={{ color:`whitesmoke`, float: `right` }}>
-                    <a href="https://www.facebook.com/TheCenturyTheater" style={ iconStyle } target="_blank"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a>
-                    <a href="https://m.me/539449256479593" style={ iconStyle } target="_blank"><FontAwesomeIcon icon={['fab', 'facebook-messenger']} /></a>
-                     <a href="https://www.instagram.com/thecenturytheater/" style={ iconStyle } target="_blank"><FontAwesomeIcon icon={['fab', 'instagram']}  /></a>
-
+                    <SocialLink link="https://www.facebook.com/TheCenturyTheater" icon={['fab', 'facebook-f']} />
+                    <SocialLink link="https://m.me/539449256479593" icon={['fab', 'facebook-messenger']} />
+                    <SocialLink link="https://www.instagram.com/thecenturytheater/" icon={['fab', 'instagram']} />
                 </span>
-
             </Container>
         </footer>
     );
