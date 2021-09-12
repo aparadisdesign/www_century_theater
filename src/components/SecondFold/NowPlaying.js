aeb303679c8fdb2c7164638a7214eca2e0a1db4c
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-12 09:59:01 -0400

    rough draft of movie details popup

--- src/components/SecondFold/NowPlaying.js
+++ src/components/SecondFold/NowPlaying.js
@@ -17,6 +17,7 @@ const opts = {
     },
 };
 
+
 const NowPlaying = (props) => {
 
     const [show, setShow] = useState(false);
@@ -38,20 +39,20 @@ const NowPlaying = (props) => {
                 </Modal.Header>
                 <Modal.Body>
                     <Card style={{ width: '100%' }}>
-                        <Card.Img variant="top" style={{height:`auto`, width: `auto`, maxHeight:`300px`, maxWidth: `200px`, margin: `10px auto`}} src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/642ed171-851c-493b-b942-81b3a29ed891/dem3j2p-2f4c686a-2c6b-4f02-9688-0b6b0db47c02.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY0MmVkMTcxLTg1MWMtNDkzYi1iOTQyLTgxYjNhMjllZDg5MVwvZGVtM2oycC0yZjRjNjg2YS0yYzZiLTRmMDItOTY4OC0wYjZiMGRiNDdjMDIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aXj3WWmpFMSzb0ZSAzyqWWDc16nhqzAVfMIOWsbrqcA" />
+                        <Card.Img variant="top" style={{height:`auto`, width: `auto`, maxHeight:`300px`, maxWidth: `200px`, margin: `10px auto`}} src={props.poster} />
                         <Card.Body>
-                            <Card.Title>Paw Patrol</Card.Title>
+                            <Card.Title>{props.title}</Card.Title>
                             <Card.Text>
                                 <ul>
-                                    <li>Rating: G</li>
-                                    <li>Running Time: 125min</li>
-                                    <li>Director: Cal Brunker</li>
-                                    <li>Producer: Jennifer Dodge</li>
+                                    <li>Rating: {props.rating}</li>
+                                    <li>Running Time: {props.running_time}</li>
+                                    <li>Director: {props.director}</li>
+                                    <li>Producer: {props.producer}</li>
                                     <li>
-                                        Rotten Tomatoes: <SocialLink link="https://www.rottentomatoes.com/m/paw_patrol_the_movie" icon={['fas', 'external-link-alt']} />
+                                        Rotten Tomatoes: <SocialLink link={props.rotten_link} icon={['fas', 'external-link-alt']} />
                                         <ul>
-                                            <li>Tomatometer: 77%</li>
-                                            <li>Audience Score: 97%</li>
+                                            <li>Tomatometer: {props.tomatometer}</li>
+                                            <li>Audience Score: {props.audience_score}</li>
                                         </ul>
                                     </li>
                                 </ul>
