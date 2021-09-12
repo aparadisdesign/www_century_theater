c2b01088024e090c1a8f2dff5335cad24b9262f7
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-12 10:20:08 -0400

    Refactoring removed repeating modal code

--- src/components/SecondFold/NowPlaying.js
+++ src/components/SecondFold/NowPlaying.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import YouTube from 'react-youtube';
-import {Button, Card, Modal} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import SocialLink from "../Footer/SocialLink/SocialLink";
 
 const styles = {
@@ -17,57 +17,52 @@ const opts = {
     },
 };
 
+const MovieList = () => {
+    let movies = [
+        {
+            title: "Paw Patrol: The Movie",
+            running_time: "1h 28m",
+            rating: "G",
+            youtube_id: "LRMTr2VZcr8",
+            poster: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/642ed171-851c-493b-b942-81b3a29ed891/dem3j2p-2f4c686a-2c6b-4f02-9688-0b6b0db47c02.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY0MmVkMTcxLTg1MWMtNDkzYi1iOTQyLTgxYjNhMjllZDg5MVwvZGVtM2oycC0yZjRjNjg2YS0yYzZiLTRmMDItOTY4OC0wYjZiMGRiNDdjMDIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aXj3WWmpFMSzb0ZSAzyqWWDc16nhqzAVfMIOWsbrqcA",
+            director: "Cal Brunker",
+            producer: "Jennifer Dodge",
+            rotten_link: "https://www.rottentomatoes.com/m/paw_patrol_the_movie",
+            tomatometer: "77%",
+            audience_score: "97%",
+        },
+        {
+            title: "Shang-Chi and the Legend of the Ten Rings",
+            running_time: "2h 12m",
+            rating: "PG-13",
+            youtube_id: "8YjFbMbfXaQ",
+            poster: "https://m.media-amazon.com/images/M/MV5BNTliYjlkNDQtMjFlNS00NjgzLWFmMWEtYmM2Mzc2Zjg3ZjEyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+            director: "Destin Daniel Cretton",
+            producer: "Kevin Feige",
+            rotten_link: "https://www.rottentomatoes.com/m/shang_chi_and_the_legend_of_the_ten_rings",
+            tomatometer: "92%",
+            audience_score: "98%",
+        }
+    ]
 
-const NowPlaying = (props) => {
-
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
+    const listItems = movies.map((movie) =>
+        <Col sm={12} md={6}>
             <div style={styles}>
-                <YouTube videoId={props.videoId} opts={opts} />
-                <Button variant="outline-light" onClick={handleShow}>More Details</Button>
+                <YouTube videoId={movie.youtube_id} opts={opts} />
+                <Button variant="outline-light" >More Details</Button>
             </div>
+        </Col>
+    );
+    return listItems
+}
 
-
-            <Modal show={show} onHide={handleClose} centered>
-                <Modal.Header>
-                    <Modal.Title>Movie Details</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Card style={{ width: '100%' }}>
-                        <Card.Img variant="top" style={{height:`auto`, width: `auto`, maxHeight:`300px`, maxWidth: `200px`, margin: `10px auto`}} src={props.poster} />
-                        <Card.Body>
-                            <Card.Title>{props.title}</Card.Title>
-                            <Card.Text>
-                                <ul>
-                                    <li>Rating: {props.rating}</li>
-                                    <li>Running Time: {props.running_time}</li>
-                                    <li>Director: {props.director}</li>
-                                    <li>Producer: {props.producer}</li>
-                                    <li>
-                                        Rotten Tomatoes: <SocialLink link={props.rotten_link} icon={['fas', 'external-link-alt']} />
-                                        <ul>
-                                            <li>Tomatometer: {props.tomatometer}</li>
-                                            <li>Audience Score: {props.audience_score}</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-
+const NowPlaying = () => {
+    return (
+        <Container>
+            <Row>
+                <MovieList/>
+            </Row>
+        </Container>
     );
 }
 
--- src/components/SecondFold/SecondFold.js
+++ src/components/SecondFold/SecondFold.js
@@ -17,45 +17,6 @@ const headerStyle = {
     fontStyle: `normal`
 }
 
-const MovieList = () => {
-    let movies = [
-        {
-            title: "Paw Patrol: The Movie",
-            running_time: "1h 28m",
-            rating: "G",
-            youtube_id: "LRMTr2VZcr8",
-            poster: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/642ed171-851c-493b-b942-81b3a29ed891/dem3j2p-2f4c686a-2c6b-4f02-9688-0b6b0db47c02.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY0MmVkMTcxLTg1MWMtNDkzYi1iOTQyLTgxYjNhMjllZDg5MVwvZGVtM2oycC0yZjRjNjg2YS0yYzZiLTRmMDItOTY4OC0wYjZiMGRiNDdjMDIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aXj3WWmpFMSzb0ZSAzyqWWDc16nhqzAVfMIOWsbrqcA",
-            director: "Cal Brunker",
-            producer: "Jennifer Dodge",
-            rotten_link: "https://www.rottentomatoes.com/m/paw_patrol_the_movie",
-            tomatometer: "77%",
-            audience_score: "97%",
-        },
-        {
-            title: "Shang-Chi and the Legend of the Ten Rings",
-            running_time: "2h 12m",
-            rating: "PG-13",
-            youtube_id: "8YjFbMbfXaQ",
-            poster: "https://m.media-amazon.com/images/M/MV5BNTliYjlkNDQtMjFlNS00NjgzLWFmMWEtYmM2Mzc2Zjg3ZjEyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-            director: "Destin Daniel Cretton",
-            producer: "Kevin Feige",
-            rotten_link: "https://www.rottentomatoes.com/m/shang_chi_and_the_legend_of_the_ten_rings",
-            tomatometer: "92%",
-            audience_score: "98%",
-        }
-    ]
-
-    const listItems = movies.map((movie) =>
-        <Col sm={12} md={6}>
-            <NowPlaying videoId={movie.youtube_id} title={movie.title} running_time={movie.running_time}
-                        rating={movie.rating} director={movie.director} producer={movie.producer} poster={movie.poster}
-                        tomatometer={movie.tomatometer} audience_score={movie.audience_score} rotten_link={movie.rotten_link}
-            />
-        </Col>
-    );
-    return listItems
-}
-
 const SecondFold = () => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
     return (
@@ -115,11 +76,7 @@ const SecondFold = () => {
                     </small>
                 </h1>
             </Container>
-            <Container>
-                <Row>
-                    <MovieList/>
-                </Row>
-            </Container>
+            <NowPlaying />
         </div>
     );
 }
