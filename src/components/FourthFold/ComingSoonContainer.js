da188e62779af82b4472cbd121ca18e25b0911f9
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-13 19:14:19 -0400

    refactoring coming soon

---
+++ src/components/FourthFold/ComingSoonContainer.js
@@ -0,0 +1,24 @@
+import React, {} from 'react';
+import 'react-ig-feed/dist/index.css'
+import {Container, Row} from "react-bootstrap";
+
+import ComingSoonTeaser from "./ComingSoonTeaser";
+
+
+const ComingSoonContainer = (props) => {
+    // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
+    return (
+        <Container>
+            <Row>
+                <ComingSoonTeaser videoId="3u9gLvLBtBQ" />
+                <ComingSoonTeaser videoId="rrwBnlYOp4g" />
+                <ComingSoonTeaser videoId="N_gD9-Oa0fg" />
+                <ComingSoonTeaser videoId="hL6R3HmQfPc" />
+                <ComingSoonTeaser videoId="8I8nMtzN05s" />
+                <ComingSoonTeaser videoId="4plqh6obZW4" />
+            </Row>
+        </Container>
+    );
+}
+
+export default ComingSoonContainer;
--- src/components/FourthFold/FourthFold.js
+++ src/components/FourthFold/FourthFold.js
@@ -1,33 +1,10 @@
 import React, {} from 'react';
 import 'react-ig-feed/dist/index.css'
-import {Col, Container, Row} from "react-bootstrap";
-import YouTube from "react-youtube";
-import ComingSoonTeaser from "./ComingSoonTeaser";
+import {Container, Row} from "react-bootstrap";
+import ComingSoonContainer from "./ComingSoonContainer";
 
 const firstLetterStyles = {
     fontSize: `15vh`,
-}
-
-const dateStyle = {
-    position: `absolute`,
-    right: `0`,
-    backgroundColor: `white`,
-    padding: `10px 25px`,
-    border: `solid thin black`,
-    fontFamily: `adobe-garamond-pro, serif`,
-    borderBottomLeftRadius: `25px`,
-    zIndex: 5,
-}
-
-const statusStyle = {
-    position: `absolute`,
-    right: `0`,
-    bottom: `0`,
-    zIndex: 5,
-    marginRight: `15px`,
-    padding: `5px 10px`,
-    backgroundColor: `#212529`,
-    color: `whitesmoke`
 }
 
 const headerStyle = {
@@ -41,15 +18,7 @@ const headerStyle = {
     fontStyle: `normal`
 }
 
-const opts = {
-    width: '100%',
-    playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 0,
-        controls: 0,
-        modestbranding: 1,
-    },
-};
+
 
 const FourthFold = (props) => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
@@ -110,16 +79,7 @@ const FourthFold = (props) => {
                     </small>
                 </h1>
             </Container>
-            <Container>
-                <Row>
-                    <ComingSoonTeaser videoId="3u9gLvLBtBQ" />
-                    <ComingSoonTeaser videoId="rrwBnlYOp4g" />
-                    <ComingSoonTeaser videoId="N_gD9-Oa0fg" />
-                    <ComingSoonTeaser videoId="hL6R3HmQfPc" />
-                    <ComingSoonTeaser videoId="8I8nMtzN05s" />
-                    <ComingSoonTeaser videoId="4plqh6obZW4" />
-                </Row>
-            </Container>
+            <ComingSoonContainer />
         </div>
     );
 }
