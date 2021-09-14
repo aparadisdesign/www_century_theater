0adeefa6c44e8afe83cc6547c5b347272757324b
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-14 14:13:01 -0400

    Menu Button Fixed Bottom RIght

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -15,55 +15,43 @@ const headerStyle = {
     fontStyle: `normal`
 }
 
+const leftLineStyle = {
+    marginRight: `5px`,
+    marginTop: `3vh`,
+    borderTop: `solid .25vh white`,
+    marginBottom: `3vh`,
+    borderBottom: `solid .25vh white`,
+    width: `auto`,
+    flexGrow: `4`,
+}
+
+const rightLineStyle = {
+    marginLeft: `5px`,
+    marginTop: `3vh`,
+    borderTop: `solid .25vh white`,
+    marginBottom: `3vh`,
+    borderBottom: `solid .25vh white`,
+    width: `auto`,
+    flexGrow: `4`
+}
+
 const HeaderSection = (props) => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
     return (
             <h2 style={props.styleOverride ? props.styleOverride : headerStyle}>
                 {props.title &&
                     <small style={{marginBottom: `10px`, display: `flex`, flexDirection: `row`}}>
-                        <span style={{
-                            marginRight: `5px`,
-                            marginTop: `3vh`,
-                            borderTop: `solid .25vh white`,
-                            marginBottom: `3vh`,
-                            borderBottom: `solid .25vh white`,
-                            width: `auto`,
-                            flexGrow: `4`
-                        }}/>
+                        <span style={props.leftLineOverride ? props.leftLineOverride : leftLineStyle}/>
                         <span style={{fontSize: `5vh`}}>{props.title}</span>
-                        <span style={{
-                            marginLeft: `5px`,
-                            marginTop: `3vh`,
-                            borderTop: `solid .25vh white`,
-                            marginBottom: `3vh`,
-                            borderBottom: `solid .25vh white`,
-                            width: `auto`,
-                            flexGrow: `4`
-                        }}/>
+                        <span style={props.rightLineOverride ? props.rightLineOverride : rightLineStyle}/>
                     </small>
                 }
                 <span style={props.firstOverride ? props.firstOverride : firstLetterStyles}>T</span>he <span style={props.firstOverride ? props.firstOverride : firstLetterStyles}>C</span>entury
                 <br/>
                 <small style={{display: `flex`, flexDirection: `row`}}>
-                    <span style={{
-                        marginRight: `5px`,
-                        marginTop: `3vh`,
-                        borderTop: `solid .25vh white`,
-                        marginBottom: `3vh`,
-                        borderBottom: `solid .25vh white`,
-                        width: `auto`,
-                        flexGrow: `4`
-                    }}/>
-                    <span style={{fontSize: `5vh`}}>Theater</span>
-                    <span style={{
-                        marginLeft: `5px`,
-                        marginTop: `3vh`,
-                        borderTop: `solid .25vh white`,
-                        marginBottom: `3vh`,
-                        borderBottom: `solid .25vh white`,
-                        width: `auto`,
-                        flexGrow: `4`
-                    }}/>
+                    <span style={props.leftLineOverride ? props.leftLineOverride : leftLineStyle}/>
+                    <span style={props.lowerFontOverride ? {fontSize: `18px`} : {fontSize:`5vh`}  }>Theater</span>
+                    <span style={props.rightLineOverride ? props.rightLineOverride : rightLineStyle}/>
                 </small>
             </h2>
     );
--- src/components/SiteMenu.js
+++ src/components/SiteMenu.js
@@ -2,7 +2,15 @@ import React, {useState} from 'react';
 import {Button, Nav} from "react-bootstrap";
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import HeaderSection from "./HeaderSection";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import {library} from "@fortawesome/fontawesome-svg-core";
 
+library.add(fas)
+
+const iconStyle = {
+    padding: `0 15px`,
+}
 const headerStyles = {
     fontSize: `16pt`,
     textTransform: `uppercase`,
@@ -10,11 +18,44 @@ const headerStyles = {
     color: `black`,
     fontFamily: `adobe-garamond-pro, serif`,
     fontWeight: `400`,
-    fontStyle: `normal`
+    fontStyle: `normal`,
+    width: `160px`,
+    margin: `auto`
 }
 
 const firstLetterStyles = {
     fontSize: `20pt`,
+}
+
+const leftLineStyle = {
+    marginRight: `5px`,
+    marginTop: `8px`,
+    borderTop: `solid .5px black`,
+    marginBottom: `23px`,
+    borderBottom: `solid .5px black`,
+    width: `auto`,
+    flexGrow: `4`,
+    height: `4px`
+
+}
+
+const rightLineStyle = {
+    marginLeft: `5px`,
+    marginTop: `8px`,
+    borderTop: `solid .5px black`,
+    marginBottom: `23px`,
+    borderBottom: `solid .5px green`,
+    width: `auto`,
+    flexGrow: `4` ,
+    height: `4px`
+}
+
+const menuButton = {
+    position: `fixed`,
+    right: `10px`,
+    bottom: `10px`,
+    borderRadius: `5px 20px 5px`,
+    zIndex: `14`,
 }
 
 const SiteMenu = (props) => {
@@ -26,19 +67,18 @@ const SiteMenu = (props) => {
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch
+            <Button variant="primary" onClick={handleShow} style={menuButton}>
+                <FontAwesomeIcon icon={['fas', 'bars']}  />
             </Button>
         <Offcanvas show={show} onHide={handleClose} placement={'end'}>
             <Offcanvas.Header closeButton>
-                <Offcanvas.Title><HeaderSection styleOverride={headerStyles} firstOverride={firstLetterStyles} /></Offcanvas.Title>
+                <Offcanvas.Title style={{width:`100%`}}><HeaderSection styleOverride={headerStyles} firstOverride={firstLetterStyles} leftLineOverride={leftLineStyle} rightLineOverride={rightLineStyle} lowerFontOverride="10px" /></Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
                 <Nav defaultActiveKey="/home" className="flex-column">
                     <Nav.Link href="/home">The Century Theater Homepage</Nav.Link>
                     <Nav.Link eventKey="link-1">About The Century Theater</Nav.Link>
                     <Nav.Link eventKey="link-2">Rent The Century Theater</Nav.Link>
-
                 </Nav>
             </Offcanvas.Body>
         </Offcanvas>
