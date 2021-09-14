263da7b3f2df355f2f1578978e2c11571f132e0f
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-09-14 07:58:02 -0400

    JSON and loop for coming soon

--- src/components/ComingSoonContainer.js
+++ src/components/ComingSoonContainer.js
@@ -4,18 +4,45 @@ import {Container, Row} from "react-bootstrap";
 
 import ComingSoonTeaser from "./ComingSoonTeaser";
 
+let comingSoonMovies = [
+    {
+        videoId: "3u9gLvLBtBQ",
+        status: "confirmed",
+        startDate: "September 17th",
+    },
+    {
+        videoId: "rrwBnlYOp4g",
+        status: "pending",
+        startDate: "October 1st",
+    },
+    {
+        videoId: "N_gD9-Oa0fg",
+        status: "pending",
+        startDate: "October 8th",
+    },
+    {
+        videoId: "hL6R3HmQfPc",
+        status: "pending",
+        startDate: "October 15th",
+    },
+    {
+        videoId: "8I8nMtzN05s",
+        status: "pending",
+        startDate: "October 22nd",
+    },
+    {
+        videoId: "4plqh6obZW4",
+        status: "pending",
+        startDate: "October 29th",
+    },
+]
 
 const ComingSoonContainer = () => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
     return (
         <Container>
             <Row>
-                <ComingSoonTeaser videoId="3u9gLvLBtBQ" />
-                <ComingSoonTeaser videoId="rrwBnlYOp4g" />
-                <ComingSoonTeaser videoId="N_gD9-Oa0fg" />
-                <ComingSoonTeaser videoId="hL6R3HmQfPc" />
-                <ComingSoonTeaser videoId="8I8nMtzN05s" />
-                <ComingSoonTeaser videoId="4plqh6obZW4" />
+                { comingSoonMovies.map((movie) => <ComingSoonTeaser videoId={movie.videoId} status={movie.status} startDate={movie.startDate} />) }
             </Row>
         </Container>
     );
--- src/components/ComingSoonTeaser.js
+++ src/components/ComingSoonTeaser.js
@@ -33,6 +33,7 @@ const statusStyle = {
     color: `whitesmoke`,
     border: `solid thin whitesmoke`,
     fontFamily: `adobe-garamond-pro, serif`,
+    textTransform: `uppercase`
 }
 
 const opts = {
@@ -49,7 +50,7 @@ const ComingSoonTeaser = (props) => {
     // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
     return (
         <Col lg={4} style={style}>
-            <h4 style={dateStyle}>October 29th</h4><h5 style={statusStyle}>Pending</h5>
+            <h4 style={dateStyle}>{props.startDate}</h4><h5 style={statusStyle}>{props.status}</h5>
             <YouTube videoId={props.videoId} opts={opts} />
         </Col>
     );
